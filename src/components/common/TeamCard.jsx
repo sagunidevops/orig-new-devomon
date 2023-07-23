77e8afe3242e5f67fbfaa94228dc04a75d999f91
sandhirradialcode <sandhirradialcode.com>
2023-07-23 20:53:08 +0530

    team card  add

--- src/components/common/TeamCard.jsx
+++ src/components/common/TeamCard.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import imdb from "../../assets/images/webp/imdb.webp";
+import imdb_icon from "../../assets/images/svg/imdb_icon.svg";
 import { LinkedIcon } from "./Icons";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
@@ -33,39 +33,43 @@ const TeamCard = ({ obj, index }) => {
     <>
       <div className="pt-16 mb-7 sm:mb-[70px] team_zoom">
         <div
-          className={` team_bg  border border-1 border-[#3B3B3B] rounded-3xl   relative   px-3 pb-4 max-w-[275px]  hover:scale-105  duration-300 ease-in-out transition-all `}>
-          <div className={` -mt-12 sm:-mt-16 flex justify-center `}>
+          className={`  relative   px-3 pb-4 max-w-[275px]  hover:scale-105  duration-300 ease-in-out transition-all `}>
+          <div className={` -mt-12 sm:-mt-16  `}>
             <img
-              className="max-w-[120px] sm:max-w-[152px] mx-auto rounded-full"
+              className="max-w-[120px] sm:max-w-[152px]"
               src={obj.img}
               alt="team"
             />
           </div>
-          <h2 className="text-white text-center font-raleway font-bold text-xs sm:text-sm md:text-base pt-4 uppercase">
-            {obj.name}
-          </h2>
-          <p className="text-center text-xs font-raleway sm:text-sm md:text-base opacity-70 text-white uppercase">
-            {obj.title}
-          </p>
-          <div
-            className={` h-[119px] px-1 overflow-auto team-scrollbar text-xs opacity-70 text-white font-poppins sm:text-sm md:text-base mt-5`}>
-            <p className="text-center">{obj.desc}</p>
+          <div className=" flex items-center gap-3">
+            <a
+              href={obj.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedinAndIMD"
+              aria-describedby="LinkedinAndIMD"
+              className=" py-4 hover:scale-105  duration-300 ease-in-out transition-all">
+              {" "}
+              {index === 2 ? (
+                <img className="max-w-[30px]" src={imdb_icon} alt="imdb" />
+              ) : (
+                <LinkedIcon />
+              )}
+            </a>
+            <div>
+              <h2 className="text-[#23CAFF] font-raleway font-bold text-xs sm:text-sm md:text-base">
+                {obj.name}
+              </h2>
+              <p className=" text-xs font-raleway sm:text-sm md:text-base opacity-70 text-[#2253F5]">
+                {obj.title}
+              </p>
+            </div>
           </div>
 
-          <a
-            href={obj.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="LinkedinAndIMD"
-            aria-describedby="LinkedinAndIMD"
-            className="flex justify-center py-4 hover:scale-105  duration-300 ease-in-out transition-all">
-            {" "}
-            {index === 2 ? (
-              <img className="max-w-[30px]" src={imdb} alt="imdb" />
-            ) : (
-              <LinkedIcon />
-            )}
-          </a>
+          <div
+            className={` text-xs opacity-70 text-white font-poppins sm:text-sm mt-2 md:mt-5`}>
+            <p>{obj.desc}</p>
+          </div>
         </div>
       </div>
     </>
