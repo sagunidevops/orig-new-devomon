d7f943078577a303fe9be2e39d48fc30abf875a9
Muskan <muskansaini758@gmail.com>
2023-08-10 15:51:00 +0530

    add api of the medium

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,23 +1,39 @@
-import Hero from './Hero';
-import About from './About';
-import AboutCard from './AboutCard';
-import LatestNews from './LatestNews';
-import Partner from './Partner';
-import TouchForm from './TouchForm';
-import SideSocialIcon from '../common/SideSocialIcon';
+import Hero from "./Hero";
+import About from "./About";
+import AboutCard from "./AboutCard";
+import LatestNews from "./LatestNews";
+import Partner from "./Partner";
+import TouchForm from "./TouchForm";
+import SideSocialIcon from "../common/SideSocialIcon";
+import { useEffect } from "react";
+import { LatestNewsHandler } from "../common/Api";
+import { NewsApi } from "../common/ApiUrls";
+import { useState } from "react";
+import moment from 'moment';
+
 
 const Homepage = () => {
+  const [latestNewsData, setLatestNewsData] = useState();
+
+  const latestNewsApiHandler = async () => {
+    const newsData = await LatestNewsHandler(NewsApi());
+    setLatestNewsData(newsData);
+  };
+  useEffect(() => {
+    latestNewsApiHandler();
+  }, []);
+console.log("latestNewsDatalatestNewsData", moment(latestNewsData&&latestNewsData[0].created).format("MMM Do"))
   return (
     <>
-    <SideSocialIcon/>
-    <Hero/>
-    <About/>
-    <AboutCard/>
-    <LatestNews/>
-    <Partner/>
-     <TouchForm/>
+      <SideSocialIcon />
+      <Hero />
+      <About />
+      <AboutCard />
+      <LatestNews />
+      <Partner />
+      <TouchForm />
     </>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
